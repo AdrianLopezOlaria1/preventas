5dc9a8f6408b4b8a1bcc2b99019d2f5204244879
JaisonOtalvaro <jaisonotalvaro@gmail.com>
2024-03-15 14:04:56 +0100

    añadido rol de usuario al formulario de registro y a los métodos implicados

--- metodos/register.php
+++ metodos/register.php
@@ -2,12 +2,13 @@
     require "clases/Usuario.php";
     $usuario = new Usuario();
     if ($_SERVER["REQUEST_METHOD"] === "POST") {
-        if (isset($_POST["nombre"]) && isset($_POST["email"]) && isset($_POST["password"])) {
+        if (isset($_POST["nombre"]) && isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["rol"])) {
             $nombre = $_POST["nombre"];
             $email = $_POST["email"];
             $password = $_POST["password"];
+            $rol = $_POST['rol'];
             
-            if($usuario->registrar($nombre, $email, $password)){
+            if($usuario->registrar($nombre, $email, $password, $rol)){
                 header("location: index.php?action=register");
             }else{
                 header("location: index.php?action=register");
