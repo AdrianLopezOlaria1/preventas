061cf221bc92c0782fcf8db65254f5a9facc75d2
JaisonOtalvaro <jaisonotalvaro@gmail.com>
2024-03-18 14:55:28 +0100

    Se agrega clase Contacto, se implementa formulario y archivos para crear contactos

--- clases/Contacto.php
+++ clases/Contacto.php
@@ -2,20 +2,34 @@
 
     require_once 'config/conexion.php';
 
-    class Cliente {
+    class Contacto {
+        private $id_cliente;
         private $nombre;
-        private $status;
+        private $email;
+        private $tel;
         private $fecha_alta;
         private $fecha_modificacion;
         private $fecha_baja;
+        
 
-
-        public function __construct($nombre = "", $status = "", $fecha_alta = "", $fecha_modificacion = "", $fecha_baja = "") {
+        public function __construct($id_cliente = "", $nombre = "", $email = "", $tel = "", 
+        $fecha_alta = "", $fecha_modificacion = "", $fecha_baja = "") {
+            $this->id_cliente = $id_cliente;
             $this->nombre = $nombre;
-            $this->status = $status;
+            $this->email = $email;
+            $this->tel = $tel;
             $this->fecha_alta = $fecha_alta;
             $this->fecha_modificacion = $fecha_modificacion;
             $this->fecha_baja = $fecha_baja;
+        }
+
+        // Getter y Setter para id_cliente
+        public function getId_cliente() {
+            return $this->id_cliente;
+        }
+
+        public function setId_cliente($id_cliente) {
+            $this->id_cliente = $id_cliente;
         }
 
         // Getter y Setter para Nombre
@@ -28,12 +42,21 @@
         }
 
         // Getter y Setter para Email
-        public function getStatus() {
-            return $this->status;
+        public function getEmail() {
+            return $this->email;
         }
 
-        public function setStatus($status) {
-            $this->status = $status;
+        public function setEmail($email) {
+            $this->email = $email;
+        }
+
+        // Getter y Setter para Tel
+        public function getTel() {
+            return $this->tel;
+        }
+
+        public function setTel($tel) {
+            $this->tel = $tel;
         }
 
         // Getter y Setter para fechas
@@ -61,72 +84,52 @@
             $this->fecha_baja = $fecha_baja;
         }
 
+        //funciones
 
-        function validarDatos($nombre) {
+        function validarDatos($nombre, $email, $tel) {
             $error = array();
-            if(!empty($nombre)){
+            if(!empty($nombre) && !is_numeric($nombre) && !preg_match("/[0-9]/", $nombre)){
                 $nombre_validado = true;
             } else {
                 $nombre_validado = false;
                 $error['nombre'] = "Insert a valid name";
             }
-/*             if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
                 $error['email'] = "Insert a valid email address";
-            } */
+            }
+            if(empty($tel) || !is_numeric($tel)){
+                $error['tel'] = "Insert a valid phone number";
+            }
             return $error;
         }
 
-    
-
-        public function nuevo($nombre) {
+        public function nuevo($id_cliente, $nombre, $email, $tel) {
             $conexion = new Conexion();
             $mysqli = $conexion->getConexion();
-            $error = $this->validarDatos($nombre);
+            $error = $this->validarDatos($nombre, $email, $tel);
             if(count($error) == 0){
-                // Verificar si el nombre ya existe en la base de datos
-                $sql_check_name = "SELECT nombre FROM clientes WHERE nombre = '$nombre'";
-                $result_check_name = mysqli_query($mysqli, $sql_check_name);
-                $row = mysqli_fetch_assoc($result_check_name);
+                // Verificar si el correo electrónico ya existe en la base de datos
+                $sql_check_email = "SELECT email FROM usuarios WHERE email = '$email'";
+                $result_check_email = mysqli_query($mysqli, $sql_check_email);
+                $row = mysqli_fetch_assoc($result_check_email);
                 if ($row) {
-                    $_SESSION['error']['nombre'] = "Error, this name is already registered";
+                    $_SESSION['error']['email'] = "Error, the email is already registered";
                     return $_SESSION['error'];
                 }
-                // Insertar Cliente en la base de datos
-                $sql = "INSERT INTO clientes VALUES(NULL,'$nombre', 'A', NOW(),
-                 NULL, NULL);";
+                // Insertar contacto en la base de datos
+                $sql = "INSERT INTO personas_contacto VALUES(NULL, $id_cliente, '$nombre', '$email', '$tel', 
+                'A', NOW(), NULL, NULL);";
                 $guardar = mysqli_query($mysqli, $sql);
                 if($guardar) {
-                    $_SESSION['completado'] = "Client has been successfully completed!";
+                    $_SESSION['completado'] = "The contact has been successfully created!";
                 } else {
-                   return false;
+                    $_SESSION['error']['general'] = "Error";
                 }
             } else {
                 $_SESSION['error'] = $error;
                 return $_SESSION['error'];
             }
         }    
-
-        public function obtenerClientes() {
-            $clientes = array();
-    
-            // Inicializar la conexión dentro del método
-            $conexion = new Conexion();
-            $mysqli = $conexion->getConexion();
-    
-            // Ejecutar la consulta SQL para obtener todos los clientes
-            $sql = "SELECT * FROM clientes";
-            $resultado = $mysqli->query($sql);
-    
-            // Verificar si la consulta se ejecutó correctamente
-            if ($resultado) {
-                // Iterar sobre los resultados y almacenar cada cliente en un array
-                while ($fila = $resultado->fetch_assoc()) {
-                    $clientes[] = $fila;
-                }
-            }
-    
-            return $clientes;
-        }
 
         function borrarErrores(){
             $borrado = false;
@@ -138,10 +141,9 @@
                 $_SESSION['completado'] = null;
                 $borrado = true;  
             }
-           
+        
             return $borrado;
         }
-  
 }
 
 
--- metodos/crearContacto.php
+++ metodos/crearContacto.php
@@ -2,12 +2,12 @@
     require "clases/Contacto.php";
     $contacto = new Contacto();
     if ($_SERVER["REQUEST_METHOD"] === "POST") {
-        if (isset($_POST["nombre"]) && isset($_POST["email"]) && isset($_POST["tel"]) ) {
+        if (isset($_POST["id_cliente"]) && isset($_POST["nombre"]) && isset($_POST["email"]) && isset($_POST["tel"]) ) {
+            $id_cliente = (int)$_POST["id_cliente"];
             $nombre = $_POST["nombre"];
             $email = $_POST["email"];
             $tel = $_POST["tel"];
-            
-            if($contacto->nuevo($nombre, $email, $tel)){
+            if($contacto->nuevo($id_cliente, $nombre, $email, $tel)){
                 header("location: index.php?action=formContacto");
             }else{
                 header("location: index.php?action=formContacto");
