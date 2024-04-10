d357ecf27c22ca6e1ce5be0cb66824968434c608
JaisonOtalvaro <jaisonotalvaro@gmail.com>
2024-04-10 11:38:06 +0200

    filtros modificados

--- views/preventas.php
+++ views/preventas.php
@@ -21,9 +21,13 @@
                             <path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"></path>
                         </svg> Filtros
                     </button>
+                    <?php if(isset($_SESSION['preventasFiltradas'])):?>
+                        <div id="filtros" class="row" style="display:block; transition: opacity 0.3s;">
+                    <?php else:?>
                         <div id="filtros" class="row" style="display:none; transition: opacity 0.3s;">
+                    <?php endif;?>   
                             <?php require 'form/formFiltros.php'?>
-                        </div>                        
+                        </div>                     
                     </div><br>                                     
                     <div class="row">
                         <div class="col-12">
