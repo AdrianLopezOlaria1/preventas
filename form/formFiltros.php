b5cd69315b8ed95b2dd816fa451616a3ea483125
jherrera <jherrera@aticsoft.com>
2024-04-11 09:55:07 +0200

    todo bien

--- form/formFiltros.php
+++ form/formFiltros.php
@@ -70,15 +70,19 @@
                 <div class="col-2 row">
                     <div class="col">
                         <br>
-                        <button type="submit" class="btn btn-primary mb-3" style="background-color: #f93215; color: white; padding: 10px 20px; border: none; transition: all 0.3s ease; border-radius: 20px; display:float;"
+                        
+                        <button type="submit" class="btn btn-primary mb-3" style="background-color: #f93215; color: white; padding: 10px 20px; border: none; transition: all 0.3s ease; border-radius: 20px;  float:left;"
                         onmouseover="this.style.backgroundColor='#2d9cfc'; this.style.transform='scale(1.1)';"
                         onmouseout="this.style.backgroundColor='#f93215'; this.style.transform='scale(1)';">Filtrar</button>
+
+
+                        <a href="index.php?action=borrarFiltro" class="btn btn-primary" style="background-color: #f93215; color: white; padding: 10px 20px; border: none; transition: all 0.3s ease; border-radius: 20px;display:float;"
+                        onmouseover="this.style.backgroundColor='#2d9cfc'; this.style.transform='scale(1.1)';"
+                        onmouseout="this.style.backgroundColor='#f93215'; this.style.transform='scale(1)';">Borrar filtros</a>
+                        
                     
 
                     
-                        <a href="index.php?action=borrarFiltro" class="btn btn-primary" style="background-color: #f93215; color: white; padding: 10px 20px; border: none; transition: all 0.3s ease; border-radius: 20px; float:left;"
-                        onmouseover="this.style.backgroundColor='#2d9cfc'; this.style.transform='scale(1.1)';"
-                        onmouseout="this.style.backgroundColor='#f93215'; this.style.transform='scale(1)';">Borrar filtros</a>
                     </div>
                 </div>
             </div>
